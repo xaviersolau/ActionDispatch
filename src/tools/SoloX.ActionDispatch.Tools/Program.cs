7dd1f1eb2d40083e52e3e31de9ae2e5d17e6b42f
Xavier Solau <xaviersolau@yahoo.fr>
2019-02-06 00:05:53 +0100

    add tools projects and remove generated xml files from git repository.

---
+++ src/tools/SoloX.ActionDispatch.Tools/Program.cs
@@ -0,0 +1,31 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="Program.cs" company="SoloX Software">
+// Copyright (c) SoloX Software. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+
+namespace SoloX.ActionDispatch.Tools
+{
+    /// <summary>
+    /// Program entry point class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main program entry point.
+        /// </summary>
+        /// <param name="args">Tools arguments.</param>
+        public static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+
+        internal void Run()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
