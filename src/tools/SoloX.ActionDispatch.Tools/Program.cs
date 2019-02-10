d2a400a2e8ee3ea2272a44c0377269bfe2cf9e08
Xavier Solau <xaviersolau@yahoo.fr>
2019-02-10 23:02:09 +0100

    update action dispatch tools setup

--- src/tools/SoloX.ActionDispatch.Tools/Program.cs
+++ src/tools/SoloX.ActionDispatch.Tools/Program.cs
@@ -6,6 +6,9 @@
 // ----------------------------------------------------------------------
 
 using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace SoloX.ActionDispatch.Tools
 {
@@ -14,18 +17,47 @@ namespace SoloX.ActionDispatch.Tools
     /// </summary>
     public class Program
     {
+        private readonly ILogger<Program> logger;
+        private readonly IConfiguration configuration;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Program"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration that contains all arguments.</param>
+        public Program(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+
+            IServiceCollection sc = new ServiceCollection();
+
+            sc.AddLogging(b => b.AddConsole());
+
+            this.Service = sc.BuildServiceProvider();
+
+            this.logger = this.Service.GetService<ILogger<Program>>();
+        }
+
+        private ServiceProvider Service { get; }
+
         /// <summary>
         /// Main program entry point.
         /// </summary>
         /// <param name="args">Tools arguments.</param>
         public static void Main(string[] args)
         {
-            new Program().Run();
+            var builder = new ConfigurationBuilder();
+            builder.AddCommandLine(args);
+            var config = builder.Build();
+
+            new Program(config).Run();
         }
 
-        internal void Run()
+        /// <summary>
+        /// Run the tools command.
+        /// </summary>
+        public void Run()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Running {this.configuration["test"]}");
         }
     }
 }
