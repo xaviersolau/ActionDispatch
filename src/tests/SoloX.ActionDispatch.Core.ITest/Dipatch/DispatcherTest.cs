36b19a0ae9e159d770f244d0019d233f67834bdd
Xavier Solau <xaviersolau@yahoo.fr>
2019-05-11 00:15:28 +0200

    Add integration test project.

---
+++ src/tests/SoloX.ActionDispatch.Core.ITest/Dipatch/DispatcherTest.cs
@@ -0,0 +1,20 @@
+// ----------------------------------------------------------------------
+// <copyright file="DispatcherTest.cs" company="SoloX Software">
+// Copyright (c) SoloX Software. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+using Xunit;
+
+namespace SoloX.ActionDispatch.Core.ITest.Dipatch
+{
+    public class DispatcherTest
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
