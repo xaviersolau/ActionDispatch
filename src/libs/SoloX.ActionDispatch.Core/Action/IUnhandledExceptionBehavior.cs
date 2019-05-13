5dba26fcaa207dcc566acaa0f7d271ff51658fb2
Xavier Solau <xaviersolau@yahoo.fr>
2019-05-13 18:31:43 +0200

    Add exception handling integration test.

---
+++ src/libs/SoloX.ActionDispatch.Core/Action/IUnhandledExceptionBehavior.cs
@@ -0,0 +1,27 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="IUnhandledExceptionBehavior.cs" company="SoloX Software">
+// Copyright (c) SoloX Software. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SoloX.ActionDispatch.Core.State;
+
+namespace SoloX.ActionDispatch.Core.Action
+{
+    /// <summary>
+    /// Unhandled exception behavior that will be used if an exception is thrown while dispatching an action.
+    /// </summary>
+    /// <typeparam name="TRootState">The state root type.</typeparam>
+    public interface IUnhandledExceptionBehavior<TRootState> : IActionBehavior<TRootState, TRootState>
+        where TRootState : IState
+    {
+        /// <summary>
+        /// Gets the unhandled exception.
+        /// </summary>
+        Exception Exception { get; }
+    }
+}
