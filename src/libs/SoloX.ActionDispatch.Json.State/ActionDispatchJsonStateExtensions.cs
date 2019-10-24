bdf64af673e5d0d06306ecea6438f5a5072879e7
Xavier Solau <xaviersolau@yahoo.fr>
2019-10-25 01:07:44 +0200

    Add new SoloX.ActionDispatch.Json.State serialization project in order to remove the expression serialization from the state serialization.

--- src/libs/SoloX.ActionDispatch.Json.State/ActionDispatchJsonStateExtensions.cs
+++ src/libs/SoloX.ActionDispatch.Json.State/ActionDispatchJsonStateExtensions.cs
@@ -1,5 +1,5 @@
 ﻿// ----------------------------------------------------------------------
-// <copyright file="ActionDispatchJsonExtensions.cs" company="SoloX Software">
+// <copyright file="ActionDispatchJsonStateExtensions.cs" company="SoloX Software">
 // Copyright (c) SoloX Software. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
@@ -11,27 +11,24 @@ using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using SoloX.ActionDispatch.Core.Action;
 using SoloX.ActionDispatch.Core.State;
-using SoloX.ActionDispatch.Json.Action.Impl;
-using SoloX.ActionDispatch.Json.State;
 using SoloX.ActionDispatch.Json.State.Impl;
 
-namespace SoloX.ActionDispatch.Json
+namespace SoloX.ActionDispatch.Json.State
 {
     /// <summary>
     /// ActionDispatch Json Extensions to setup the Json serialization for action and state.
     /// </summary>
-    public static class ActionDispatchJsonExtensions
+    public static class ActionDispatchJsonStateExtensions
     {
         /// <summary>
         /// Setup the Service collection in order to inject Action and State serializer Json implementation.
         /// </summary>
         /// <param name="services">The services collection instance to setup.</param>
         /// <returns>The service collection given as input.</returns>
-        public static IServiceCollection AddActionDispatchJsonSupport(this IServiceCollection services)
+        public static IServiceCollection AddActionDispatchStateJsonSupport(this IServiceCollection services)
         {
             return services
-                .AddActionDispatchStateJsonSupport()
-                .AddSingleton<IActionSerializer, JsonActionSerializer>();
+                .AddSingleton<IStateSerializer, JsonStateSerializer>();
         }
     }
 }
