f831e41630ed88322ada7ab72a67c3077d47d0ab
Xavier Solau <xaviersolau@yahoo.fr>
2019-10-28 23:26:31 +0100

    Moved test action behavior to json unit test project.

---
+++ src/tests/SoloX.ActionDispatch.Json.UTest/Action/Basic/SyncBehavior.cs
@@ -0,0 +1,31 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="SyncBehavior.cs" company="SoloX Software">
+// Copyright (c) SoloX Software. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SoloX.ActionDispatch.Core.Action;
+using SoloX.ActionDispatch.Core.Sample.State.Basic;
+using SoloX.ActionDispatch.Core.State;
+
+namespace SoloX.ActionDispatch.Json.UTest.Action.Basic
+{
+    public class SyncBehavior : IActionBehavior<IStateA>
+    {
+        public SyncBehavior(string someValue)
+        {
+            this.SomeValue = someValue;
+        }
+
+        public string SomeValue { get; }
+
+        public void Apply(IStateContainer<IStateA> stateContainer)
+        {
+            // Nothing to do for now.
+        }
+    }
+}
