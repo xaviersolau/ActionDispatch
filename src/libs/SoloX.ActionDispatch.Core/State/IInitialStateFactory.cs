593f38f504d40a3630ec9374157c2ede9a1adbe4
Xavier Solau <xaviersolau@yahoo.fr>
2019-10-31 19:00:46 +0100

    Add an injectable IInitialStateFactory interface and use it to setup the action dispatcher.

---
+++ src/libs/SoloX.ActionDispatch.Core/State/IInitialStateFactory.cs
@@ -0,0 +1,27 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="IInitialStateFactory.cs" company="SoloX Software">
+// Copyright (c) SoloX Software. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoloX.ActionDispatch.Core.State
+{
+    /// <summary>
+    /// Initial state factory. It is used to initialize the state of the dispatcher.
+    /// </summary>
+    /// <typeparam name="TRootState">Type of the root state object on witch actions will apply.</typeparam>
+    public interface IInitialStateFactory<TRootState>
+        where TRootState : IState
+    {
+        /// <summary>
+        /// Create the initial state of the dispatcher.
+        /// </summary>
+        /// <returns>The created initial state.</returns>
+        TRootState Create();
+    }
+}
